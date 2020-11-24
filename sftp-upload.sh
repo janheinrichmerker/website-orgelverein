6c497e7d5754aa52cd3e1127ff62aabf5dd0e5d1
Jan Heinrich Reimer <heinrich@reimer.family>
2020-11-24 23:47:49 +0100

    Update deployment

--- sftp-upload.sh
+++ sftp-upload.sh
@@ -10,15 +10,15 @@ echo "$key" >"$temp/key"
 chmod 700 "$temp/key"
 
 function sftp-command() {
-  echo "$@" | sftp -b - -i "$temp/key" "$user@$host"
+  echo "$@" | sftp -b - -i "$temp/key" "$user@$host" 2>&1
 }
 
 function sftp-test-connection() {
-  sftp-command "exit" >/dev/null 2>&1
+  sftp-command "exit" >/dev/null
 }
 
 function sftp-full-list() {
-  sftp-command "ls -fl $1" 2>&1 |
+  sftp-command "ls -fl $1" |
     sed -n '/sftp>/,$p' |
     tail -n +2
 }
@@ -56,13 +56,13 @@ function sftp-list-directories-recursively() {
 
 function sftp-mirror-local() {
   echo "Finding remote files recursively…"
-  remote_files=$(sftp-list-files-recursively "$path" | tr -s " \t\r\n" "\n" | sort)
+  remote_files=$(sftp-list-files-recursively "$path" | tr -s " \t\r\n" "\n" | sort | uniq)
   echo "Finding remote directories recursively…"
-  remote_directories=$(sftp-list-directories-recursively "$path" | tr -s " \t\r\n" "\n" | sort)
+  remote_directories=$(sftp-list-directories-recursively "$path" | tr -s " \t\r\n" "\n" | sort | uniq)
   echo "Found $(echo "$remote_files" | wc -l) remote files, $(echo "$remote_directories" | wc -l) remote directories."
 
-  local_files=$(find public -type f -printf '%P\n' | awk '{print "'"$path"'/"$0}' | sort)
-  local_directories=$(find public -type d -printf '%P\n' | awk '{print "'"$path"'/"$0}' | sort)
+  local_files=$(find public -type f -printf '%P\n' | awk '{print "'"$path"'/"$0}' | sort | uniq)
+  local_directories=$(find public -type d -printf '%P\n' | awk '{print "'"$path"'/"$0}' | sort | uniq)
   echo "Found $(echo "$local_files" | wc -l) local files, $(echo "$local_directories" | wc -l) local directories."
 
   removable_files=$(comm -23 <(echo "$remote_files") <(echo "$local_files") | tr "\n" " ")
@@ -70,15 +70,15 @@ function sftp-mirror-local() {
   echo "Found $(echo "$removable_files" | wc -l) remote-only files, $(echo "$removable_directories" | wc -l) remote-only directories."
 
   echo "Uploading local files…"
-  sftp-command put -R public "$path" >/dev/null 2>&1
+  sftp-command put -R public "$path" >/dev/null
 
   echo "Removing remote-only files…"
   for file in $removable_files; do
-    sftp-command rm "$file" >/dev/null 2>&1
+    sftp-command rm "$file" >/dev/null
   done
   echo "Removing remote-only directories…"
   for directory in $removable_directories; do
-    sftp-command rmdir "$directory" >/dev/null 2>&1
+    sftp-command rmdir "$directory" >/dev/null
   done
 }
 
@@ -90,5 +90,4 @@ fi
 sftp-mirror-local
 
 rm -r "$temp"
-
-exit
+exit 0
